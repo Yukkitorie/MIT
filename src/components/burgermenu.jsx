8e5c1874f230020c71324411ff7daa1df8ea4e96
Yukkitorie <manastormmillhaus@gmail.com>
2024-08-05 21:56:45 +0100

    burger menu react pre-alpha

---
+++ src/components/burgermenu.jsx
@@ -0,0 +1,14 @@
+// import React, { useEffect, useState } from 'react';
+
+// const burgermenu = () => {
+//     return(
+//         <div>
+//             <nav className='-navbar'>
+//                 <div className="burger-btn">
+//                     <span/>
+//                 </div>
+//             </nav>
+//         </div>
+//     )
+// }
+
